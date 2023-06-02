a342cd11cd76b268020d7359b5b2c766aa69d641
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-06-02 20:36:16 -0300

    Contexto Entre Rodas

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
+import { useState } from 'react'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
+
 
 export function App() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-          <Router />
-          <GlobalStyle />
-        </BrowserRouter>  
+          <BrowserRouter>
+          <CyclesContextProvider>
+              <Router />
+          </CyclesContextProvider>
+            <GlobalStyle />
+          </BrowserRouter>  
       </ThemeProvider>
     </>
   )
