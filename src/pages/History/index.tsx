7a44620ff19ff2119acb35ad7417661f8430f3c0
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-06-04 09:30:37 -0300

    Formatando data, date-fns

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,7 @@
 import { useContext} from "react";
 import { CyclesContext } from "../../contexts/CyclesContext";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/esm/locale/pt-BR/index.js";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History(){
@@ -25,7 +27,10 @@ export function History(){
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} Minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>{formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR
+                  })}</td>
                   <td>
                     {cycle.finishedDate && <Status statusColor="green">Concluido</Status>}
                     {cycle.interruptedDate && <Status statusColor="red">Interrompido</Status>}
